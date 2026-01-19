913d0c72d7ecbb24c11b064f31cc6a1c4476f5d3
S100000 <redg7338@gmail.com>
2026-01-19 16:10:39 -0300

    HanoiTowerTroubleAgain

---
+++ CompleteSearch/HanoiTowerTrobleAgain.cpp
@@ -0,0 +1,42 @@
+#include <bits\stdc++.h>
+
+using namespace std;
+
+int T, N, total;
+vector<int> peg(100, 0);
+
+void Backtracking(int v, int p);
+{
+    if(p == N) return;
+    if(peg[p] == 0)
+    {
+        peg[p] = v;
+        tot ++;
+        return Backtracking(v+1, p);
+    }
+
+    for(int i = 0; i <= p; i ++)
+    {
+        int root = (int) sqrt(peg[i] + v);
+        if(root * root == peg[i] + v)
+        {
+            peg[i] = v;
+            tot ++;
+            return Backtracking(v + 1, p);
+        }
+    }
+
+    Backtracking(v + 1, p);
+}
+
+int main()
+{
+    scanf("%d", &T);
+    while(T--)
+    {
+        scanf("%d, &N");
+        tot = 0;
+        Backtracking(1, 0);
+        cout << tot << endl;
+    }
+}
