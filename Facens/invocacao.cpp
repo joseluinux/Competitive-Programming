0f5633612c88b3f78f61ac5f7d8598b5ca7c0784
joseluinux <redg7338@gmail.com>
2026-06-04 01:18:15 -0300

    invocacao

---
+++ Facens/invocacao.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i ++)
+    {
+        int e;
+        cin >> e;
+        cout << "yu gi oh";
+        for(int j = 0; j < e*2; j++)
+        {
+            cout << "h";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
